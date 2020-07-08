2267137ce3371bc95f7a8b792b77a76ab21c9669
Gustavo Jeuken <gjeuken@gmail.com>
2020-07-08 12:04:34 +0200

    Fix typo

--- src/components/Lobby.js
+++ src/components/Lobby.js
@@ -115,7 +115,7 @@ export default function Lobby({
                 credentials,
                 leaveGame,
                 setGameID,
-                setPlayerID,:
+                setPlayerID,
                 setCredentials,
               }}
             />
