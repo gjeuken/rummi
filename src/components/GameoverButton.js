0b7cb5b1dc94193fcd40f02f0697181a60960645
Gustavo Jeuken <gjeuken@gmail.com>
2020-07-08 12:22:43 +0200

    Added code for gameover button

---
+++ src/components/GameoverButton.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Button } from 'react-bootstrap';
+import { useHistory } from "react-router-dom"
+
+const GameoverButton = () => {
+		const gameID = localStorage.getItem('gameID')
+		const playerID = localStorage.getItem('playerID')
+		const credentials = localStorage.getItem('credentials')
+
+		const leaveGame = () => {
+				fetch(`${process.env.REACT_APP_GAME_SERVER}games/rummikub/${gameID}/leave`, {
+						method: 'POST',
+						headers: {
+								'Content-Type': 'application/json'
+						},
+						body: JSON.stringify({
+								playerID: playerID,
+								credentials: credentials
+						})
+				})
+		}
+
+		const handleLeave = () => {
+				localStorage.removeItem('gameID')
+				localStorage.removeItem('playerID')
+				localStorage.removeItem('credentials')
+				leaveGame()
+		}
+
+		const history = useHistory()
+		
+		const handleClick = () => {
+				handleLeave()
+				history.push('/lobby')
+		}
+		
+		return (
+				<Button onClick={handleClick}>
+				Return to lobby
+				</Button>
+		);
+}
+
+export default GameoverButton
