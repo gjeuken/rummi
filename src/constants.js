42b67803e946f8fce20a2520595eda22a14c7972
Gustavo Jeuken <gjeuken@gmail.com>
2020-07-08 11:41:02 +0200

    Wider board and rack

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ export const ItemTypes = {
   TILE: 'tile'
 }
 
-export const BOARD_WIDTH = 16
+export const BOARD_WIDTH = 24
 export const BOARD_HEIGHT = 10
-export const RACK_WIDTH = 8
+export const RACK_WIDTH = 13
 export const RACK_HEIGHT = 4
